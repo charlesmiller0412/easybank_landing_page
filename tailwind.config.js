f55b1a4b0e177cb54082c1b5e85f23c9eefce63e
Charles Miller <charles.miller.0412@gmail.com>
2022-08-31 14:56:04 -0500

    updated font & color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
         "./components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        screens: {
+            xl: "1440px",
+        },
+        extend: {
+            screens: {
+                xs: "375px",
+            },
+            colors: {
+                "dark-blue": "hsl(233, 26%, 24%)",
+                "lime-green": "hsl(136, 65%, 51%)",
+                "bright-cyan": "hsl(192, 70%, 51%)",
+                "gray-blue": "hsl(233, 8%, 62%)",
+                "light-gray-blue": "hsl(220, 16%, 96%)",
+                "light-gray": "hsl(0, 0%, 98%)",
+                "off-white": "hsl(0, 0%, 100%)",
+            },
+        },
     },
     plugins: [],
 };
